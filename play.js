cfe3705222b415c7a3796517002779d2c8e1f8f5
Christian Girling <girling.christian@gmail.com>
2022-01-19 17:00:13 -0800

    Added input and handleUserInput

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (button) => {
+  const stdout = process.stdout;
+  if (button === '\u0003') {
+    stdout.write("You have quit. You are a quitter.\n")
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();
