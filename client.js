967b408d86a32e8964956cf4f99169997ed69056
Christian Girling <girling.christian@gmail.com>
2022-01-19 16:43:16 -0800

    Commented out hard-coded movement

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const net = require('net');
-
+const host = '165.227.47.243'
+const port = 50541
 const connect = function() {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-    
+    host,
+    port
   });
 
   // interpret incoming data as text
@@ -13,8 +13,14 @@ const connect = function() {
   conn.on('connect', () => {
     console.log("Connected to the server!");
     conn.write("Name: BRR")
+  //   setInterval(func = () => {
+  //     conn.write("Move: up")
+  //   }, 1000);
 
-  });
+  //   setInterval(func = () => {
+  //     conn.write("Move: right")
+  //   }, 500);
+  // });
   
   conn.on('data', data => {
     console.log(data);
